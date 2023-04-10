76ade8372d44b777d92f8e3b017ced5de3eb7810
Vatrala <vatralanaska@gmail.com>
2023-04-10 21:11:23 +0300

    commit

---
+++ script.js
@@ -0,0 +1,88 @@
+// const age = prompt('Please, enter your age?');
+// let result;
+// if (age >= 0 && age <= 11) {
+//     result = "You are child!"
+// } else if (age >= 12 && age <= 17) {
+//     result = "You are teenager!"
+// } else if (age >= 18 && age <= 59) {
+//     result = "You are adult!"
+// } else if (age >= 60) {
+//     result = "You are senior!"
+// } else {
+//     result = "You entered incorrect information"
+// }
+//
+// alert(result);
+//
+// const userSymbol = prompt("Please, enter a number between 0 and 9");
+// let resultSymbol;
+// //
+// switch (userSymbol) {
+//     case '0':
+//         resultSymbol = ")";
+//         break;
+//     case '1':
+//         resultSymbol = "!";
+//         break;
+//     case '2':
+//         resultSymbol = "@";
+//         break;
+//     case '3':
+//         resultSymbol = "#";
+//         break;
+//     case '4':
+//         resultSymbol = "$";
+//         break;
+//     case '5':
+//         resultSymbol = "%";
+//         break;
+//     case '6':
+//         resultSymbol = "^";
+//         break;
+//     case '7':
+//         resultSymbol = "&";
+//         break;
+//     case '8':
+//         resultSymbol = "*";
+//         break;
+//     case '9':
+//         resultSymbol = "(";
+//         break;
+//     default:
+//         resultSymbol = "You entered incorrect number!!!"
+// }
+//
+// alert(`Your symbol is " ${resultSymbol} "`);
+//
+// //SHORT FORM IF ELSE
+//
+// const symbol = prompt("Please, enter a number between 0 and 9");
+//
+// symbol === '0'
+//     ? resultSymbol = ')'
+//     : symbol === '1'
+//         ? resultSymbol = '!'
+//         : symbol === '2'
+//             ? resultSymbol = "@"
+//             : symbol === '3'
+//                 ? resultSymbol = "#"
+//                 : symbol === '4'
+//                     ? resultSymbol = "$"
+//                     : symbol === '5'
+//                         ? resultSymbol = "%"
+//                         : symbol === '6'
+//                             ? resultSymbol = "^"
+//                             : symbol === '7'
+//                                 ? resultSymbol = "&"
+//                                 : symbol === '8'
+//                                     ? resultSymbol = "*"
+//                                     : symbol === '9'
+//                                         ? resultSymbol = "("
+//                                         : resultSymbol = "You entered incorrect symbol"
+// alert(`Your symbol is " ${resultSymbol} "`)
+
+
+
+
+
+
