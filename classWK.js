9be95ccb15e1c873ab380f160b6a7ed75ececf42
Vatrala <vatralanaska@gmail.com>
2023-05-03 11:37:02 +0300

    add

--- classWK.js
+++ classWK.js
@@ -1,6 +1,20 @@
-const button = document.querySelector('.btn');
+// const button = document.querySelector('.btn');
+//
+// button.addEventListener('click', () => {
+//     let bar = document.querySelector('.bar');
+//     bar.value = (bar.value === bar.max) ? 0 : bar.value + 10;
+// });
 
-button.addEventListener('click', () => {
-    let bar = document.querySelector('.bar');
-    bar.value = (bar.value === bar.max) ? 0 : bar.value + 10;
+
+
+$(document).ready(function(){
+    $('.your-class').slick({
+        arrows: true,
+        // autoplay: true,
+        // autoplaySpeed: 0,
+        // speed: 7000,
+        // cssEase: "linear",
+        // pauseOnHover: true,
+        dots: true,
+});
 });
