2179cca4f32a09b4034f5b9a466255a553b3451a
Vatrala <vatralanaska@gmail.com>
2023-05-06 09:59:22 +0300

    add

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-const promise = new Promise((resolve, reject) => {
-
-    resolve('resolve')
-    reject('reject')
-});
-
-promise.then((res) => console.log(res));
+// const promise = new Promise((resolve, reject) => {
+//
+//     resolve('resolve')
+//     reject('reject')
+// });
+//
+// promise.then((res) => console.log(res));
 
 // console.log(promise);
+
+const API = 'https://swapi.dev/api/planets/';
+
+fetch(API, {
+})
+    .then((res) => res.json())
+    .then((data) => console.log(data))
